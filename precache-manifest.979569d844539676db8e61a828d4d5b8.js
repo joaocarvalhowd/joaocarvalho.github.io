d61628951211b9304f14d9c0bf001774800301ea
João Carvalho <oi@joaocarvalhowd.com.br>
2019-04-08 20:58:30 -0300

    Updates

---
+++ precache-manifest.979569d844539676db8e61a828d4d5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "32325d744218ee32e3c1",
+    "url": "/static/js/main.609837a5.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "32325d744218ee32e3c1",
+    "url": "/static/css/main.bdd37752.chunk.css"
+  },
+  {
+    "revision": "ea4cbfce50a8b48d6c85bafcd78924ab",
+    "url": "/index.html"
+  }
+];
