9c16f9b3d17c18e8d2dbe2f017794be90d1874bc
João Carvalho <oi@joaocarvalhowd.com.br>
2019-04-08 21:00:38 -0300

    Updates

---
+++ precache-manifest.90c31eb96f7d21b661f50135e2225686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "738c5e760cdb5d1c1801",
+    "url": "/website/static/js/runtime~main.0439b053.js"
+  },
+  {
+    "revision": "75d9ed1d4f27388172ad",
+    "url": "/website/static/js/main.c0c044a2.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/website/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "75d9ed1d4f27388172ad",
+    "url": "/website/static/css/main.bdd37752.chunk.css"
+  },
+  {
+    "revision": "ed9526c5a45ec902d94f73a58e006926",
+    "url": "/website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.979569d844539676db8e61a828d4d5b8.js"
+  "/website/precache-manifest.90c31eb96f7d21b661f50135e2225686.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
